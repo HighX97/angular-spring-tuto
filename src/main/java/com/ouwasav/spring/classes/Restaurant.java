884ab2ad5ee4e99cb949b64910752ae5b2cad176
Loïc ORTOLE <loort@p-mpl-framboise.dhcp.mpl.intranet>
2017-02-03 16:12:30 +0100

    Tuto Spring - Classes before dependency injection

---
+++ src/main/java/com/ouwasav/spring/classes/Restaurant.java
@@ -0,0 +1,11 @@
+package com.ouwasav.spring.classes;
+
+public class Restaurant {
+	
+	Tea tea = new Tea() ;
+	
+	public void prepareHotDrink() {
+		tea.prepareTea();
+	}
+
+}
---
+++ src/main/java/com/ouwasav/spring/main/MainSpring.java
@@ -0,0 +1,13 @@
+package com.ouwasav.spring.main;
+
+import com.ouwasav.spring.classes.* ;
+
+public class MainSpring {
+	
+	public static void main(String [] args)
+	{
+		Restaurant r = new Restaurant() ;
+		r.prepareHotDrink();
+	}
+
+}
