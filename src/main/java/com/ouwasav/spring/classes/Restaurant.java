0718715b0b0744648d934bda461a6e6b2298ff5f
Loïc ORTOLE <loort@p-mpl-framboise.dhcp.mpl.intranet>
2017-02-03 16:58:49 +0100

    Tuto Spring - Spring GetBean

--- src/main/java/com/ouwasav/spring/classes/Restaurant.java
+++ src/main/java/com/ouwasav/spring/classes/Restaurant.java
@@ -4,6 +4,15 @@ public class Restaurant {
 	
 	//Tea tea = new Tea() ;
 	IHotDrink hotDrink ;
+	private String welcomeNote ;
+	
+	
+	public void greetCustomer(){
+		System.out.println(welcomeNote);
+	}
+	
+	public Restaurant() {
+	}
 	
 	public Restaurant(IHotDrink hotDrink) {
 		this.hotDrink = hotDrink;
@@ -12,6 +21,14 @@ public class Restaurant {
 	public void prepareHotDrink() {
 		//tea.prepareTea();
 		hotDrink.prepareTea();
+	}
+
+	public String getWelcomeNote() {
+		return welcomeNote;
+	}
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
 	}
 
 }
--- src/main/java/com/ouwasav/spring/main/MainSpring.java
+++ src/main/java/com/ouwasav/spring/main/MainSpring.java
@@ -1,14 +1,25 @@
 package com.ouwasav.spring.main;
 
+import org.springframework.context.ApplicationContext ;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.ouwasav.spring.classes.* ;
 
 public class MainSpring {
 	
+	private static ApplicationContext context;
+
 	public static void main(String [] args)
 	{
 		Tea t = new Tea();
 		Restaurant r = new Restaurant(t) ;
 		r.prepareHotDrink();
+		
+		context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+		
+		Restaurant resto =  (Restaurant) context.getBean("restaurantBean");
+		
+		resto.greetCustomer();
 	}
 
 }
