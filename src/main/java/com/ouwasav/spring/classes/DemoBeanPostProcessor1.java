625330d958bed54119771deb20d717f4020e8f50
HighX97 <loic.ortole@gmail.com>
2017-02-06 11:35:09 +0100

    Implementing an Ordered interface in a BeanPostProcessor class

--- src/main/java/com/ouwasav/spring/classes/DemoBeanPostProcessor1.java
+++ src/main/java/com/ouwasav/spring/classes/DemoBeanPostProcessor1.java
@@ -4,25 +4,25 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.Ordered;
 
-public class DemoBeanPostProcessor implements BeanPostProcessor  , Ordered
+public class DemoBeanPostProcessor1 implements BeanPostProcessor , Ordered
 {
 
-	
+
 	public Object postProcessAfterInitialization(Object arg0, String arg1) throws BeansException {
 		// TODO Auto-generated method stub
-		System.err.println("postProcessAfterInitialization (i.e just after init life cycle event ) of : " + arg1);
+		System.err.println("DemoBeanPostProcessor1 postProcessAfterInitialization (i.e just after init life cycle event ) of : " + arg1);
 		return arg0;
 	}
 
 	public Object postProcessBeforeInitialization(Object arg0, String arg1) throws BeansException {
 		// TODO Auto-generated method stub
-		System.err.println("postProcessBeforeInitialization (i.e just after instanciante and before init life cycle event )" + arg1);
+		System.err.println("DemoBeanPostProcessor1 postProcessBeforeInitialization (i.e just after instanciante and before init life cycle event )" + arg1);
 		return arg0;
 	}
 
 	public int getOrder() {
 		// TODO Auto-generated method stub
-		return 1;
+		return 0;
 	}
 
 }
