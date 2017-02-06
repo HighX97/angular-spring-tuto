302bd43fed2e5eb0907f36e4f6ddf69dc46c9c04
HighX97 <loic.ortole@gmail.com>
2017-02-06 10:08:14 +0100

    Bean Scope

--- src/main/java/com/ouwasav/spring/main/MainSpring.java
+++ src/main/java/com/ouwasav/spring/main/MainSpring.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.ouwasav.spring.classes.* ;
 
 public class MainSpring {
-	
+
 	private static ApplicationContext context;
 
 	public static void main(String [] args)
@@ -14,14 +14,30 @@ public class MainSpring {
 		Tea t = new Tea();
 		Restaurant r = new Restaurant(t) ;
 		r.prepareHotDrink();
-		
+
 		context = new ClassPathXmlApplicationContext("SpringConfig.xml");
-		
+
 		Restaurant resto =  (Restaurant) context.getBean("restaurantBean");
-		
+		resto.setWelcomeNote("welcomeNote restaunt 0");
 		resto.greetCustomer();
 		resto.prepareHotDrink();
 		resto.displayWaitersNames();
+
+		Restaurant resto1 =  (Restaurant) context.getBean("restaurantBean");
+		resto1.greetCustomer();
+		resto1.prepareHotDrink();
+		resto1.displayWaitersNames();
+
+		Restaurant resto2 =  (Restaurant) context.getBean("restaurantBeanSing");
+		resto2.setWelcomeNote("welcomeNote restaunt 2");
+		resto2.greetCustomer();
+		resto2.prepareHotDrink();
+		resto2.displayWaitersNames();
+
+		Restaurant resto3 =  (Restaurant) context.getBean("restaurantBeanSing");
+		resto3.greetCustomer();
+		resto3.prepareHotDrink();
+		resto3.displayWaitersNames();
 	}
 
 }
