6f21abd327bf98ef3d25d0ea2e7ea88b01160d8a
HighX97 <loic.ortole@gmail.com>
2017-02-06 17:44:38 +0100

    RESTful Web Services with Spring - GET - POST

--- src/main/java/com/ouwasav/spring/controllers/RestaurantController.java
+++ src/main/java/com/ouwasav/spring/controllers/RestaurantController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,27 +16,53 @@ import com.ouwasav.spring.models.Restaurant;
 import com.ouwasav.spring.models.Tea;
 
 @RestController
-public class RestaurantController {
+public class RestaurantController 
+{
 	
-	@RequestMapping(value = "/restau" , method=RequestMethod.GET , produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-	public Restaurant getRestaurant(
-			@RequestParam(value ="name" ,defaultValue="Resto") String name)
-	{
-		Tea t = new Tea();
-		Restaurant r = new Restaurant(t) ;
-		r.prepareHotDrink();
-		return r;
-	}
-
-    @RequestMapping(value = "/list" , method=RequestMethod.GET , produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
-    public List<Restaurant> test1(@RequestParam(value="name", defaultValue="World") String name) {
-        List<Restaurant> rslt = new ArrayList<Restaurant>();
+    List<Restaurant> rslt;
+    public RestaurantController()
+    {
+    	rslt = new ArrayList<Restaurant>();
         rslt.add(new Restaurant("A"));
         rslt.add(new Restaurant("B"));
         rslt.add(new Restaurant("C"));
         rslt.add(new Restaurant("D"));
         rslt.add(new Restaurant("E"));
-    	return rslt;
+    }
+	
+	@RequestMapping(value = "/restau" , method=RequestMethod.GET , produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Restaurant> getRestaurant(
+			@RequestParam(value ="name" ,defaultValue="Resto") String name)
+	{
+		Tea t = new Tea();
+		Restaurant r = new Restaurant(t) ;
+		r.prepareHotDrink();
+		return new ResponseEntity<Restaurant>(r, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/restau/{id}" , method=RequestMethod.GET , produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Restaurant> getRestaurantById(
+			@PathVariable("id") int id )
+	{
+		Tea t = new Tea();
+		Restaurant r = new Restaurant(t) ;
+		r.setName("Restaurant"+id);
+		r.prepareHotDrink();
+		return new ResponseEntity<Restaurant>(r, HttpStatus.OK);
+	}
+
+    @RequestMapping(value = "/restau/list" , method=RequestMethod.GET , produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Restaurant>> getAllRestaurant(@RequestParam(value="name", defaultValue="World") String name) {
+    	return new ResponseEntity<List<Restaurant>>(rslt, HttpStatus.OK);
+    }
+    
+    
+    @RequestMapping(value = "/restau" , method=RequestMethod.POST , consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE ,produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Restaurant>> createRestaurant(@RequestBody Restaurant resto) 
+    {
+    	System.err.println(resto);
+    	this.rslt.add(resto);
+    	return new ResponseEntity<List<Restaurant>>(this.rslt, HttpStatus.CREATED);
     }
 
 }
