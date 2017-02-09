606cdf7345107c972760dedf234acb00e9a7a90b
HighX97 <loic.ortole@gmail.com>
2017-02-09 18:01:47 +0100

    Angular Fundamentals

--- src/main/resources/data/hsqldb/data.sql
+++ src/main/resources/data/hsqldb/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO Utilisateur (nom ,phone,	mail,password ) values ('','','','');
+INSERT INTO Utilisateur (nom ,phone,mail,password ) values ('Danaé','0123456789','danae@smile.fr','password');
+INSERT INTO Utilisateur (nom ,phone,mail,password ) values ('Marc','0123456789','danae@smile.fr','password');
+INSERT INTO Utilisateur (nom ,phone,mail,password ) values ('Guillaume','0123456789','danae@smile.fr','password');
+INSERT INTO Utilisateur (nom ,phone,mail,password ) values ('Jean François','0123456789','danae@smile.fr','password');
+INSERT INTO Utilisateur (nom ,phone,mail,password ) values ('Jean François','0123456789','danae@smile.fr','password');
